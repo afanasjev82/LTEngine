06b9241370ed662cbd4078110b553ac15c4eca2f
Piero Toffanin <pt@masseranolabs.com>
2025-04-29 16:43:32 -0400

    Add UI from LT

--- ltengine/src/main.rs
+++ ltengine/src/main.rs
@@ -1,39 +1,82 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web_static_files::ResourceFiles;
 use clap::Parser;
 
 mod languages;
 use languages::LANGUAGES;
 
+mod models;
+use models::MODELS;
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// Hostname to bind to
-    #[arg(long, default_value = "127.0.0.1")]
+    #[arg(long, default_value = "0.0.0.0")]
     host: String,
 
     /// Port to bind to
     #[arg(short, long, default_value_t = 5000)]
     port: u16,
+
+    /// Model to use
+    #[arg(long, value_parser = MODELS.keys().collect::<Vec<_>>(), default_value = "gemma3-1b")]
+    model: String,
+
+    /// Path to .gguf model file
+    #[arg(long, default_value = "")]
+    model_file: String,
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("LT Engine is running")
-}
+
+// #[get("/")]
+// async fn index() -> impl Responder {
+//     HttpResponse::Ok().body("LT Engine is running")
+// }
 
 #[get("/languages")]
 async fn get_languages() -> impl Responder {
     HttpResponse::Ok().json(&*LANGUAGES)
 }
 
+#[get("/frontend/settings")]
+async fn get_frontend_settings() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "apiKeys": false,
+        "charLimit": 5000,
+        "filesTranslation": false,
+        "frontendTimeout": 1000,
+        "keyRequired": false,
+        "language": {
+            "source": {
+                "code": "auto",
+                "name": "Auto Detect"
+            },
+            "target": {
+                "code": "en",
+                "name": "English"
+            }
+        },
+        "suggestions": false,
+        "supportedFilesFormat": []
+    }))
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     HttpServer::new(|| {
+        let generated = generate();
+
         App::new()
-            .service(index)
+            // .service(index)
             .service(get_languages)
+            .service(get_frontend_settings)
+            .service(ResourceFiles::new("/", generated))
     })
     .bind((args.host, args.port))?
     .run()
