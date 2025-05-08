5ac2e70639cebc8e0ea35b62e9dd572da26cd6af
Piero Toffanin <pt@masseranolabs.com>
2025-05-07 21:25:47 -0400

    Error handling in JSON working

---
+++ ltengine/src/error_response.rs
@@ -0,0 +1,36 @@
+use serde::Serialize;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use serde_json::to_string_pretty;
+use actix_web::{ResponseError, HttpResponse, body::BoxBody};
+use actix_web::http::StatusCode;
+
+#[derive(Debug, Serialize)]
+pub struct ErrorResponse {
+  pub error: String,
+  pub status: u16
+}
+
+impl Display for ErrorResponse {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+      write!(f, "{}", to_string_pretty(self).unwrap())
+    }
+}
+
+impl ResponseError for ErrorResponse {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::from_u16(self.status).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        HttpResponse::build(self.status_code()).json(serde_json::json!({"error": self.error.clone()}))
+    }
+}
+
+impl From<actix_web::Error> for ErrorResponse {
+    fn from(err: actix_web::Error) -> Self {
+        ErrorResponse {
+            error: err.to_string(),
+            status: err.as_response_error().status_code().as_u16(),
+        }
+    }
+}
