0f27ec1c67f67121a4e15c88e59aae8f043a04f7
Piero Toffanin <pt@masseranolabs.com>
2025-05-09 16:27:29 -0400

    More models

--- ltengine/src/models.rs
+++ ltengine/src/models.rs
@@ -23,6 +23,8 @@ pub static MODELS: once_cell::sync::Lazy<HashMap<&'static str, HuggingFace>> = o
     let mut m = HashMap::new();
     m.insert("gemma3-1b", HuggingFace { repo: "libretranslate/gemma3", model: "gemma-3-1b-it-q4_0.gguf" });
     m.insert("gemma3-4b", HuggingFace { repo: "libretranslate/gemma3", model: "gemma-3-4b-it-q4_0.gguf" });
+    m.insert("gemma3-12b", HuggingFace { repo: "libretranslate/gemma3", model: "gemma-3-12b-it-q4_0.gguf" });
+    m.insert("gemma3-27b", HuggingFace { repo: "libretranslate/gemma3", model: "gemma-3-27b-it-q4_0.gguf" });
     m
 });
 
